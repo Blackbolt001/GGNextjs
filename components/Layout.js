6a39d1d3633d218f899ccda0c8863dca4dbb28f2
Joseph Gossett <105749663+Blackbolt001@users.noreply.github.com>
2023-11-01 07:11:56 -0400

    push

--- components/Layout.js
+++ components/Layout.js
@@ -8,15 +8,15 @@ export default function Layout({children}) {
   const { data: session } = useSession()
  if (!session) {
   return (
-    <div className=" mx-auto bg-[url('https://www.hdwallpapers.in/download/tunnel_lights_reflection_on_road_black_background_hd_black_background-HD.jpg')]  justify-center bg-no-repeat bg-cover bg-center min-h-screen  flex flex-wrap">
+    <div className=" mx-auto bg-[url('https://img1.wallspic.com/previews/0/3/5/8/3/138530/138530-urbanarea-water-light-city-blue-x350.jpg')]  justify-center bg-no-repeat bg-cover bg-center min-h-screen  flex flex-wrap">
       <div className="bg-black bg-cover h-96 w-full bg-center p-24 flex">
-        <h1 className="text-white font-bold text-6xl hover:text-neon-blue">Georgia Goose
-        <span className="text-black-400 hover:text-neon-yellow"> Designs </span> 
+        <h1 className="text-white font-bold text-6xl hover:text-neon-blue blur-sm hover:blur-none duration-1000">Georgia Goose
+        <span className="text-black-400 hover:text-neon-yellow hover:blur-none blur-sm duration-700"> Designs </span> 
         </h1>
         <div>
-        <h2 className="text-silver-600 mx-4 text-center font-bold text-3x1 hover:text-neon-blue"> <ul>Backend Dashboard platforms</ul>
+        <h2 className="text-silver-600 mx-4 text-center font-bold text-3x1 blur-sm hover:text-candlelight-200 hover:blur-none duration-1000"> <ul>Backend Dashboard platforms</ul>
         <ul>
-          <li className=" text-stone-400 hover:text-dodger-blue-500">putting businesses back in charge.
+          <li className=" text-stone-400 hover:text-silver-100">putting businesses back in charge.
           </li>
           </ul> 
         </h2>
@@ -32,7 +32,7 @@ export default function Layout({children}) {
 
  }
   return (
-   <div className="bg-[url('https://4kwallpapers.com/images/wallpapers/windows-11-dark-mode-abstract-background-black-background-2560x2560-8710.jpg')] justify-center bg-no-repeat bg-cover bg-center min-h-screen flex">
+   <div className="bg-[url('https://img1.wallspic.com/previews/0/3/5/8/3/138530/138530-urbanarea-water-light-city-blue-x350.jpg')] justify-center bg-no-repeat bg-cover bg-center min-h-screen flex">
     <Nav/>
     <div className="flex-grow bg-black mt-2 mr-2 mb-2 rounded-lg p-4">
     <div>{children}</div>
--- components/Nav.js
+++ components/Nav.js
@@ -2,15 +2,15 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 export default function Nav() {
-const inactiveLink = 'flex gap-2  flex opacity-80 text-stone-200  text-bold rounded-l-lg hover:opacity-100 hover:text-neon-blue';
-const activeLink = inactiveLink+ ' bg-[url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuLsmshbDKtxK7jFfXIxv_EaJHZiuHuRLjUvXHl39ZOS0WHB1wunErwKv9JLteasJfUhU&usqp=CAU)]  bg-origin-border bg-left... bg-no-repeat text-neon-blue hover:opacity-100';
+const inactiveLink = 'flex gap-2  flex opacity-80 text-stone-200  text-bold rounded-l-lg hover:opacity-100 hover:text-neon-pink hover:bg-black-950';
+const activeLink = inactiveLink+ ' bg-dodger-silver-200 text-neon-blue hover:opacity-100';
 const router =useRouter();
 const {pathname} = router;
 
 
 
     return(
-        <aside className=" bg-[url('https://www.webex.com/content/dam/wbx/us/images/virtual-backgrounds/Virtual_Background-Dark.jpg?5o9b8zgrSo')] justify-center bg-no-repeat bg-cover bg-center text-black-200 font-bold text-2xl p-4 pr-0 opacity-90 ">
+        <aside className="  text-black-200 font-bold text-2xl p-4 pr-0 opacity-90 ">
             <Link href={'/'} className="flex gap-1 mb-4 mr-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
         <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z" />
