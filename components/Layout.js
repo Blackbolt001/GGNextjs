26dd48ded0b8973d65f7f06d7303749f7281c660
Joseph Gossett <105749663+Blackbolt001@users.noreply.github.com>
2023-11-03 02:19:12 -0400

    errors

--- components/Layout.js
+++ components/Layout.js
@@ -8,14 +8,14 @@ export default function Layout({children}) {
   const { data: session } = useSession()
  if (!session) {
   return (
-    <div className=" relative mx-auto bg-[url('https://img1.wallspic.com/previews/0/3/5/8/3/138530/138530-urbanarea-water-light-city-blue-x350.jpg')] z-1 justify-center  bg-no-repeat top center bg-center min-h-screen flex flex-wrap">
+    <div className=" relative mx-auto bg-[url('https://img1.wallspic.com/previews/0/3/5/8/3/138530/138530-urbanarea-water-light-city-blue-x350.jpg')]  justify-center  bg-no-repeat top center bg-center min-h-screen flex flex-wrap">
       <h1></h1>
       <div className="   p- flex flex-auto">
-        <h1 className="text-neon-blue font-bold text-6xl hover:text-neon-blue  hover:blur-none duration-1000 z-5">Georgia Goose
-        <span className="text-black-400 hover:text-neon-yellow hover:blur-none blur-sm duration-700 z-3"> Designs </span> 
+        <h1 className="text-neon-blue font-bold text-6xl hover:text-neon-blue  hover:blur-none duration-1000 ">Georgia Goose
+        <span className="text-black-400 hover:text-neon-yellow hover:blur-none blur-sm duration-700 "> Designs </span> 
         </h1>
         <div>
-        <h2 className="text-silver-600 mx-4 text-center font-bold text-3x1 blur-sm hover:text-candlelight-200 hover:blur-none duration-1000 z-2"> <ul>Backend Dashboard platforms</ul>
+        <h2 className="text-silver-600 mx-4 text-center font-bold text-3x1 blur-sm hover:text-candlelight-200 hover:blur-none duration-1000 "> <ul>Backend Dashboard platforms</ul>
         <ul>
           <li className=" text-stone-400 hover:text-silver-100 z-2">putting businesses back in charge.
           </li>
@@ -25,7 +25,7 @@ export default function Layout({children}) {
    </div>
 
    
-   <div className=" flex items-center justify-center bg-neon-pink absolute bottom-40">
+   <div className=" flex items-center justify-center bg-neon-pink bottom-40">
    <button type="submit" onClick={() => signIn('google')} className=" align-center justify-center text-stone-100 transition-1 transition-all transition-100 font-bold border-2 rounded-md border-l-neon-200 px-10 py-3 bg-transparent opacity-80 hover:bg-stone-300 hover:shadow-lg bg-gradient-to-r from-stone-700 to-stone-800 tracking-widest ring-2 ring-stone-200 hover:brightness-125 hover:contrast-100 hover:saturate-200 hover:shadow-stone-100">Connect
     </button>
    </div>
@@ -35,7 +35,7 @@ export default function Layout({children}) {
  }
   return (
 
-   <div className=" relative bg-[url('https://img1.wallspic.com/previews/0/3/5/8/3/138530/138530-urbanarea-water-light-city-blue-x350.jpg')] justify-center bg-no-repeat bg-black-950 bg-center min-h-screen flex">
+   <div className=" relative bg-[url('https://img1.wallspic.com/previews/0/3/5/8/3/138530/138530-urbanarea-water-light-city-blue-x350.jpg')] justify-center bg-no-repeat bg-center min-h-screen flex">
     <Nav/>
     <div className="flex-grow bg-black mt-2 mr-2 mb-2 rounded-lg p-4">
     <div>{children}</div>
