e7c8b903a91090202b4e74de59a639087d8400a7
Joseph Gossett <105749663+Blackbolt001@users.noreply.github.com>
2023-11-07 23:51:34 -0500

    mobile

--- components/Layout.js
+++ components/Layout.js
@@ -15,25 +15,19 @@ export default function Layout({children}) {
       
       <h1></h1>
       <div className="   p-12 flex flex-auto">
-        <Draggable>
         <h1 className="text-neon-blue font-bold text-6xl   hover:blur-none duration-1000 "> <span className="text-neon-pink saturate-150 font-bold">Georgia</span> Goose
         <span className="text-black-400 hover:text-neon-yellow hover:blur-none blur-sm duration-700 ">
           <p className="  hover:shadow-lg tracking-widest ring-2 ring-neon-green  hover:contrast-100 hover:saturate-200">Designs</p></span> 
         </h1>
-        </Draggable>
-        <Draggable>
         <div className="bg-stone-800 saturate-50 py-20">
         <h2 className="text-neon-yellow saturate-200 mx-4 text-center font-bold text-3x1  hover:text-black-950 "> <ul>Backend Dashboard platforms</ul>
         </h2>
         </div>
-        </Draggable>
 </div>
-
    <div className=" flex items-center justify-center bottom-40">
    <button type="submit" onClick={() => signIn('google')} className=" align-center justify-center text-stone-100 transition-1 transition-all transition-100 font-bold border-2 rounded-md border-l-neon-200 px-10 py-3 b opacity-80 hover:bg-stone-300 hover:shadow-lg bg-gradient-to-r from-stone-700 to-stone-800 tracking-widest ring-2 ring-stone-200 hover:brightness-125 hover:contrast-100 hover:saturate-200 hover:shadow-stone-100">Connect
     </button>
    </div>
-
    </div>
    </div>
       
@@ -42,12 +36,13 @@ export default function Layout({children}) {
  }
   return (
     <div className=" min-h-screen bg-grey-grey flex flex-col">
-    <Nav/>
+  
     <div className=" p-4 bg-silver-200 rounded-lg">
       <h1 className="text-black-700 font-bold text-5xl hover:blur-none duration-1000 ">Georgia
       <span className=" text-neon-pink"> Goose</span>
       <span className="text-black-700 hover:text-neon-yellow hover:blur-none blur-sm duration-700  ">  Designs</span> 
-         </h1>
+         </h1> 
+         <Nav/>
     <div className="flex-grow bg-silver-200 mt-2 mr-2 mb-2 rounded-lg p-4">
     <div>{children}</div>
   </div>
