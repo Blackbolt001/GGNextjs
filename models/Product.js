a2fb221b24836edd937ed284994dc0de5b9ae719
Joseph Gossett <105749663+Blackbolt001@users.noreply.github.com>
2023-11-12 09:00:35 -0500

    adding time stamp

--- models/Product.js
+++ models/Product.js
@@ -7,6 +7,8 @@ const ProductSchema = new Schema({
     images:[{type:String }],
     category: {type:mongoose.Types.ObjectId, ref:'Category'},
     properties:{type:Object},
+}, {
+    timestamps:true,
 
 });
 
